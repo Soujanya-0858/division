6eda784a7f73ea068a4ba8974fe095ea21159bca
Soujanya <anipeddisoujanya@gmail.com>
2023-08-29 21:42:55 +0530

    findingdivisibility

---
+++ division/src/division/divisibilityrule.java
@@ -0,0 +1,17 @@
+package division;
+
+public class divisibilityrule {
+
+	public static void main(String[] args) {
+		int n=25;
+		if(n%5==0)
+		{
+			System.out.println("divisible");
+		}
+		else {
+			System.out.println("not");
+		}
+
+	}
+
+}
